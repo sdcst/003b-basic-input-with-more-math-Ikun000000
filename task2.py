bf028a3d3b33c1622bd1b946a424373e1c35a147
Ikun000000 <159072923+Ikun000000@users.noreply.github.com>
2024-04-12 12:51:23 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,4 @@
 ca = float(input("Enter the the amount of Canadian dollars"))
 d = ca*1.25
 d1 = round(d,2)
-print(f"That is worth {format(d1,'.2f')} dollars")
+print(f"That is worth {format(d1,'.2f')} US dollars")
